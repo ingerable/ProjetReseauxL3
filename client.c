1e75ba200716ccce0aee620746c46f2aa28e94b4
jules van assche <j.vanassche@etu.unistra.fr>
2017-12-06 23:46:16 +0100

    avancement structure message / création buffer

--- client.c
+++ client.c
@@ -35,6 +35,17 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include "server.h"
+#include "client.h"
+
+unsigned short int addToBuff(char *bufCursor,char *s,unsigned short int length,unsigned short int cursor)
+{
+  for (unsigned short int i= 0; i < length; i++)
+  {
+    bufCursor[cursor+i] = s[i];
+  }
+  cursor = cursor + length+1;
+  return cursor;
+}
 
 
 int main(int argc, char **argv)
@@ -44,9 +55,9 @@ int main(int argc, char **argv)
     struct sockaddr_in6 dest;
 
     // check the number of args on command line
-	if(argc != 4)
+	if(argc != 5)
     {
-        printf("USAGE: %s ./client IP PORT COMMANDE HASH [IP]\n", argv[0]);
+        printf("USAGE: %s IP PORT COMMANDE HASH [IP]\n", argv[0]);
         exit(-1);
     }
 
@@ -65,11 +76,19 @@ int main(int argc, char **argv)
 	// get addr from command line and convert it
 	if(inet_pton(AF_INET6, argv[1], &dest.sin6_addr) != 1)
 	{
-    printf("ERROR\n");
 		perror("inet_pton");
 		close(sockfd);
 		exit(EXIT_FAILURE);
 	}
+
+  //prepare buffer
+  char buf[1024];
+  unsigned short int cursor=0;
+
+  cursor = addToBuff(buf,argv[3],3,cursor);
+  printf("%s\n",buf );
+  cursor = addToBuff(buf,argv[4],(unsigned)strlen(argv[4]),cursor);
+  printf("%s\n",buf );
 
 	// send string
 	if(sendto(sockfd, argv[3], strlen(argv[3]), 0
@@ -84,4 +103,17 @@ int main(int argc, char **argv)
 	close(sockfd);
 
 	return 0;
+}
+
+char *addField(char *buf, const char *field)
+{
+    while(*field!='\0')
+    {
+      buf++;
+      *buf = *field;
+      field++;
+    }
+    *buf='\0';
+    buf++;
+    return buf;
 }
---
+++ client.h
@@ -0,0 +1,1 @@
+unsigned short int addToBuff(char *bufCursor,char *s,unsigned short int length,unsigned short int cursor);
