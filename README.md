a99b5fac926183902029d31104ffa3015d1fa106
jules van assche <j.vanassche@etu.unistra.fr>
2017-12-10 20:27:22 +0100

    ajout page man(pas encore finie)

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Ne pas oublier
 
-- [] Lors de l'insertion dans la hashtable, vérifier que l'on ne dépasse pas la capacité, si oui réallouer en doublant la taille
+- [x] Lors de l'insertion dans la hashtable, vérifier que l'on ne dépasse pas la capacité, si oui réallouer en doublant la taille
 
-- [] pareil pour l'insertion d'un serveur dans la serveur table
+- [x] pareil pour l'insertion d'un serveur dans la serveur table
 
 - [] mutex autour des variables globales (curseurs, lors de suppresion d'un server/hash)
 
--- test_get_put.sh
+++ test_get_put.sh
@@ -1,6 +1,12 @@
-./client ::1 40000 put jeanlouis 2001:0db8:85a3:0000:0000:8a2e:0370:6666
-./client ::1 40000 put jeanlouis 2001:0db8:85a3:0000:0000:8a2e:0370:1111
-./client ::1 40000 put jeanlouis 2001:0db8:85a3:0000:0000:8a2e:0370:1234
-./client ::1 40000 get jeanlouis
+#!/bin/sh
+
+./server ::1 40003 > test.txt&
+./client ::1 40003 put hash1example 2001:0db8:85a3:0000:0000:8a2e:0370:6666 > test.txt
+./client ::1 40003 put hash1example 2001:0db8:85a3:0000:0000:8a2e:0370:1111 > test.txt
+./client ::1 40003 get hash1example > test.txt
+
+kill $!
+
+
 
 
